807b99e48bd2aba257c095db9f623b4d9a0e8c8c
mmb1337 <boustani.mehdi1@gmail.com>
2025-05-16 16:11:50 +0100

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -11,3 +11,9 @@ SELECT
     RANK() OVER (ORDER BY COUNT(*) DESC) AS property_rank
 FROM bookings
 GROUP BY property_id;
+SELECT
+    property_id,
+    COUNT(*) AS total_bookings,
+    ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS rank
+FROM bookings
+GROUP BY property_id;
