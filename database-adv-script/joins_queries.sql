35f02ce65656b28ebf10769d4756778545c8d2b7
mmb1337 <boustani.mehdi1@gmail.com>
2025-05-16 16:09:05 +0100

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -19,6 +19,7 @@ SELECT
     reviews.comment
 FROM properties
 LEFT JOIN reviews ON properties.id = reviews.property_id;
+ORDER BY properties.id;
 
 -- FULL OUTER JOIN: Get all users and all bookings, even if unmatched
 SELECT
